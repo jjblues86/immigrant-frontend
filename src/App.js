f80d27d7ce10561d68c97c6547b8e6d6e6240224
Alhagie Bai Cham <alhagiebcham.ee@gmail.com>
2020-07-14 18:12:57 -0700

    TASK-10 installed axios

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import styles from './App.module.css';
+import axios from 'axios';
 
 function App() {
+
     return (
         <div className={styles.App}>
             <p>This is the beginning</p>
