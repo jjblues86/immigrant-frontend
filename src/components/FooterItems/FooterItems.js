ef19cc490d3012b9e0f26d33c655882dd3cf3c4b
Alhagie Bai Cham <alhagiebaicham@Alhagies-MacBook-Pro.local>
2020-08-21 18:55:33 -0700

    install fontawesome and tested it

--- src/components/FooterItems/FooterItems.js
+++ src/components/FooterItems/FooterItems.js
@@ -12,7 +12,12 @@ const footerItems = () => {
                     <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
                     is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
                 </p>
+                <div>
+                    <i className="fa fa-home"/>
+                    <i className="fab fa-facebook"/>
+                </div>
             </div>
+
         </footer>
     )
 };
