1c45ab97a372091fe4a354ea7c6a82119a3cc99a
Alhagie Bai Cham <alhagiebaicham@Alhagies-MacBook-Pro.local>
2020-08-24 14:53:42 -0700

    open contact form

--- src/components/FooterItems/FooterItems.js
+++ src/components/FooterItems/FooterItems.js
@@ -1,25 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {Redirect} from "react-router";
 
-const footerItems = () => {
+class FooterItems extends Component{
 
-    return (
-        <footer className='footer'>
-            <div className="content has-text-centered">
-                <button className='btn btn-primary'>Contact Us</button>
-                <p>
-                    <strong>Immigrant dreams</strong> by <a href="http://theimmigrantdream.com/">JJ & ABC</a>. The source code is
-                    licensed
-                    <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
-                    is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
-                </p>
-                <div>
-                    <i className="fa fa-home"/>
-                    <i className="fab fa-facebook"/>
+    contactUsHandler = () => {
+        console.log('in contact us handler')
+        this.props.history.push({pathname: '/contact'})
+    }
+    render() {
+        return (
+            <footer className='footer'>
+                <div className="content has-text-centered">
+                    <button className='btn btn-info'
+                            onClick={this.contactUsHandler}>Contact Us</button>
+                    <p>
+                        <strong>Immigrant dreams</strong> by <a href="http://theimmigrantdream.com/">JJ & ABC</a>. The
+                        source code is
+                        licensed
+                        <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
+                        is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+                    </p>
+                    <div>
+                        <i className="fa fa-home"/>
+                        <i className="fab fa-facebook"/>
+                    </div>
                 </div>
-            </div>
-
-        </footer>
-    )
+            </footer>
+        )
+    }
 };
 
-export default footerItems;
+export default FooterItems;
