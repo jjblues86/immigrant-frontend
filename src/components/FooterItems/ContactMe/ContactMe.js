0f1e5da74cb37231e0fedc6ee949d06cd4cb47c1
Alhagie Bai Cham <alhagiebaicham@Alhagies-MacBook-Pro.local>
2020-09-26 00:58:24 -0400

    class based formik

--- src/components/FooterItems/ContactMe/ContactMe.js
+++ src/components/FooterItems/ContactMe/ContactMe.js
@@ -1,73 +1,117 @@
-import React from 'react';
-import {useFormik} from "formik";
+import React, {Component} from 'react';
+import {Formik} from "formik";
 import * as Yup from 'yup';
+import axios from 'axios';
 import style from './ContactMe.module.css'
 
-const ContactMe = () => {
-
-    const formik = useFormik({
-        initialValues: {
+class ContactMe extends Component {
+    state = {
+        values: {
             firstName: '',
             lastName: '',
-            email: '',
-            message: ''
+            message: '',
+            email: ''
         },
-        validationSchema: Yup.object({
-            firstName: Yup.string().max(15, 'Must be 15 characters or less').required('First name is required'),
-            lastName: Yup.string().max(15, 'Must be 15 characters or less').required('First name is required'),
-            email: Yup.string().email('Invalid email address').required('Email is required'),
-            message: Yup.string().min(15, 'Must be 15 characters').max(200, 'Max characters is 200').required('Message is required'),
-        }),
-        onSubmit: values => {
-            // console.log(values);
-            // function we need to execute when a form is submitted
-            formik.handleReset({values: ''})
-        },
-    })
+        emailResponse: null
+    }
 
-    return (
-        <div className='col-lg-6 col-md-8 col-sm-12 m-auto'>
-            <p>Please fill out the form below to contact us about the site.</p>
-            <form
-                onSubmit={formik.handleSubmit}
+    contactDataHandler = (values) => {
+        const url = 'https://fyq7ujacf9.execute-api.us-east-1.amazonaws.com/dev/contact-me';
+        this.setState({
+            values: {
+                firstName: values.firstName,
+                lastName: values.lastName,
+                email: values.email,
+                message: values.message
+            }
+        });
+        const data = this.state.values;
+        axios.post(url, data)
+            .then(result => {
+                if (result.data.errorMessage) {
+                    this.setState({emailResponse: 'Please try again in 10 minutes. Maintenance!'})
+                } else {
+                    this.setState({emailResponse: result.data})
+                }
 
-                style={{alignContent: "center"}}>
+            })
+            .catch(err => {
+                console.log(err)
+                this.setState({emailResponse: 'Please try again!'})
+            })
+    }
 
-                <div className='form-group'>
-                    <label htmlFor="firstName">First Name</label>
-                    <input className='form-control' id="firstName" type="text"
-                           {...formik.getFieldProps('firstName')}/>
-                    {formik.touched.firstName && formik.errors.firstName ?
-                        (<div className={style.Error}>{formik.errors.firstName}</div>) : null}
-                </div>
-                <div className='form-group'>
-                    <label htmlFor="lastName">Last Name</label>
-                    <input className='form-control' id="lastName" type="text"
-                           {...formik.getFieldProps('lastName')}/>
-                    {formik.touched.lastName && formik.errors.lastName ?
-                        (<div className={style.Error}>{formik.errors.lastName}</div>) : null}
-                </div>
+    render() {
 
-                <div className='form-group'>
-                    <label htmlFor='email'>Email Address</label>
-                    <input className='form-control' id='email' type='email'
-                           {...formik.getFieldProps('email')}/>
-                    {formik.touched.email && formik.errors.email ?
-                        (<div className={style.Error}>{formik.errors.email}</div>) : null}
-                </div>
+        let response = null;
+        if (this.state.emailResponse) {
+            response = <div style={{textAlign: 'center'}}>{this.state.emailResponse}</div>
+        }
+        return (
+            <div>
+                {response}
+                <Formik
+                    initialValues={{
+                        firstName: '',
+                        lastName: '',
+                        email: '',
+                        message: ''
+                    }}
+                    validationSchema={Yup.object({
+                        firstName: Yup.string().max(15, 'Must be 15 characters or less').required('First name is required'),
+                        lastName: Yup.string().max(15, 'Must be 15 characters or less').required('Last name is required'),
+                        email: Yup.string().email('Invalid email address').required('email is required'),
+                        message: Yup.string().min(10, 'At least 10 characters long').max(200, 'Max characters is 200').required('Enter a message'),
+                    })}
+                    onSubmit={(values, {resetForm}) => {
+                        this.contactDataHandler(values)
+                        console.log(this.state.values)
+                        resetForm({values: ''})
+                    }}>
+                    {formik => (<form
+                        onSubmit={formik.handleSubmit}
+                        className='col-lg-6 col-md-8 col-sm-12 m-auto'
+                        style={{alignContent: "center"}}>
+                        <div className='form-group'>
+                            <label htmlFor="firstName">First Name</label>
+                            <input className='form-control' id="firstName" type="text"
+                                   onChange={formik.handleChange}
+                                   value={formik.values.firstName}/>
+                            {formik.touched.firstName && formik.errors.firstName ?
+                                (<div className={style.Error}>{formik.errors.firstName}</div>) : null}
+                        </div>
 
-                <div className='form-group'>
-                    <label htmlFor="message">Message</label>
-                    <textarea className='form-control' id="message"
-                           {...formik.getFieldProps('message')}/>
-                    {formik.touched.message && formik.errors.message ?
-                        (<div className={style.Error}>{formik.errors.message}</div>) : null}
-                </div>
+                        <div className='form-group'>
+                            <label htmlFor="lastName">Last Name</label>
+                            <input className='form-control' id="lastName" type="text"
+                                   onChange={formik.handleChange}
+                                   value={formik.values.lastName}/>
+                            {formik.touched.lastName && formik.errors.lastName ?
+                                (<div className={style.Error}>{formik.errors.lastName}</div>) : null}
+                        </div>
 
-                <button type='submit' className='btn btn-success'>Submit</button>
-            </form>
-        </div>
-    );
-};
+                        <div className='form-group'>
+                            <label htmlFor='email'>Email Address</label>
+                            <input className='form-control' id='email' type='email'
+                                   onChange={formik.handleChange}
+                                   value={formik.values.email}/>
+                            {formik.touched.email && formik.errors.email ?
+                                (<div className={style.Error}>{formik.errors.email}</div>) : null}
+                        </div>
+                        <div className='form-group'>
+                            <label htmlFor="message">Message</label>
+                            <textarea className='form-control' id="message"
+                                      onChange={formik.handleChange}
+                                      value={formik.values.message}/>
+                            {formik.touched.message && formik.errors.message ?
+                                (<div className={style.Error}>{formik.errors.message}</div>) : null}
+                        </div>
+                        <button type='submit' className='btn btn-success'>Submit</button>
+                    </form>)}
+                </Formik>
+            </div>
+        );
+    };
+}
 
 export default ContactMe;
