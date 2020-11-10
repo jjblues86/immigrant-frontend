0b254210f42eb9c961463eace69aa484e3bd9244
Alhagie Bai Cham <alhagiebcham.ee@gmail.com>
2020-11-10 10:28:18 -0800

    adjusted stories

--- src/components/Main/Stories/Stories.js
+++ src/components/Main/Stories/Stories.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import picture from "../../../assets/immidream.jpeg";
 import Card from "../Card/Card";
 import * as ReactBootstrap from "react-bootstrap";
+import styles from "./Stories.module.css";
 
 const config = require('../../../config.json');
 
@@ -31,17 +32,23 @@ class Stories extends Component {
         const immigrantsList = immigrants.length ?
             (immigrants.map(immigrant => {
                 return (
-                    <Card key={immigrant.id}
-                          photo={picture}
-                          tag={immigrant.storyTitle}
-                          tagId={'/story/' + immigrant.id}
+                    <Card
+                        cardClass={styles.Card}
+                        imageClass={styles.Image}
+                        textClass={styles.Title}
+                        linkClass={styles.Link}
+
+                        key={immigrant.id}
+                        photo={picture}
+                        tag={immigrant.storyTitle}
+                        tagId={'/story/' + immigrant.id}
                     />
                 )
             })) : (
                 <div className="has-text-centered">
                     <ReactBootstrap.Spinner animation="grow" size="sm" variant="primary"/>
                 </div>
-                )
+            )
         return (
             <div>
                 <div className="row">
