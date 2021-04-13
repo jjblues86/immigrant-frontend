96e71abb6532d604e4810393dea6db7451eae780
Jerome Joof <joofjero@amazon.com>
2021-04-13 10:16:20 -0700

    removing my aws credentials from github

--- src/components/Main/Card/ImageCard.js
+++ src/components/Main/Card/ImageCard.js
@@ -1,10 +1,6 @@
 import React, {Component} from 'react';
 import AWS from 'aws-sdk';
 
-AWS.config.update({accessKeyId: "AKIAYIEYNSW2K3UJJGN5",
-    secretAccessKey: "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",
-region: "eu-west-1"});
-
 const awsS3Bucket = new AWS.S3();
 
 const params = {
