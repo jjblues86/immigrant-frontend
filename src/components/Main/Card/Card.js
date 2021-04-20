ccb468559ce87f55c2c33c6b8015cce2ee60dcfc
Alhagie Bai Cham <cham207388@gmail.com>
2021-04-20 16:37:25 -0700

    edited card classes

--- src/components/Main/Card/Card.js
+++ src/components/Main/Card/Card.js
@@ -11,10 +11,10 @@ class Card extends Component {
     render() {
 
         return (
-            <section onClick={this.storyData} className={style.cards}>
+            <section onClick={this.storyData} className={style.Cards}>
                 <img src={this.props.photo} alt={this.props.name}/>
-                <h1 className={style.name}>{this.props.name}</h1>
-                <h2 className={style.tag}>{this.props.tag}</h2>
+                <h1 className={style.Name}>{this.props.name}</h1>
+                <h2 className={style.Tag}>{this.props.tag}</h2>
                 <h3>{this.props.desc}</h3>
                 <div>
                     <Link to={this.props.tagId}>
@@ -27,35 +27,3 @@ class Card extends Component {
 }
 
 export default withRouter(Card);
-
-
-// <Container className={classes.cardGrid} maxWidth="md">
-//     {/* End hero unit */}
-//     <Grid container spacing={4}>
-//         {cards.map(card => (
-//             <Grid item key={card} xs={12} sm={6} md={4}>
-//                 <Card className={classes.card}>
-//                     <CardMedia
-//                         className={classes.cardMedia}
-//                         image={card.img}
-//                         title="Image title"
-//                     />
-//                     <CardContent className={classes.cardContent}>
-//                         <Typography gutterBottom variant="h5" component="h2">
-//                             Heading
-//                         </Typography>
-//                         <Typography>{card.desc}</Typography>
-//                     </CardContent>
-//                     <CardActions>
-//                         <Button size="small" color="primary">
-//                             View
-//                         </Button>
-//                         <Button size="small" color="primary">
-//                             Edit
-//                         </Button>
-//                     </CardActions>
-//                 </Card>
-//             </Grid>
-//         ))}
-//     </Grid>
-// </Container>
